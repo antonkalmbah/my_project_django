d6be9c2a2c012fead53bba10fa939f646ad72f79
Anton <anton.kalmbah@yandex.ru>
2021-09-19 11:44:50 +0300

    Модуль D.6. Задание D6.5

--- project/news/apps.py
+++ project/news/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news'
+
+
+class AppointmentConfig(AppConfig):
+    name = 'appointment'
+
+# нам надо переопределить метод ready, чтобы при готовности нашего приложения импортировался модуль
+# со всеми функциями обработчиками
+    def ready(self):
+        import project.news.signals
